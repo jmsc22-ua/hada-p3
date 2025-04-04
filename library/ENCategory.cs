067668deda26948302fde01b873a3a9325bab574
Nayara <nnr6@gcloud.ua.es>
2025-04-04 21:30:43 +0200

    50507209Z ENCategory.cs creado

---
+++ library/ENCategory.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+    public class ENCategory
+    {
+        private string _name;
+        private int _id;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("name");
+                }
+                _name = value;
+            }
+        }
+
+        public int Id
+        {
+            get { return _id; }
+            set
+            {
+                _id = value;
+            }
+        }
+
+        public ENCategory()
+        {
+            Name = " ";
+            Id = 0;
+        }
+        public ENCategory(string name, int id)
+        {
+            Name = name;
+            Id = id;
+        }
+
+        public bool Read()
+        {
+            CADCategory cad = new CADCategory();
+            return cad.Read(this);
+        }
+
+        public List<ENCategory> ReadAll()
+        {
+            CADCategory cad = new CADCategory();
+            return cad.ReadAll();
+        }
+
+    }
+}
